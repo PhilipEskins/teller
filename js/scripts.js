801269ce4b436af2ae61ab18d971a2729b3e131b
Dino Nguyen and Philip Eskins <dino@>
2019-01-23 14:29:48 -0800

    try out new logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 var luckyCount = 0;
 var unluckyCount = 0;
 
-function messageGenerator (lucky, unlucky) {
-  var message;
-  if (lucky > unlucky) {
-    message = "You are lucky";
-  } else if (lucky < unlucky) {
-    message = "You are unlucky";
-  } else if (lucky === unlucky) {
-    message = "You are neutral";
-  }
- return message;
-}
+// function counter() {
+//   if (blarg2 === "black-cat") {
+//     alert("works");
+//   }
+// }
+
+// function messageGenerator (lucky, unlucky) {
+//   var message;
+//   if (lucky > unlucky) {
+//     message = "You are lucky";
+//   } else if (lucky < unlucky) {
+//     message = "You are unlucky";
+//   } else if (lucky === unlucky) {
+//     message = "You are neutral";
+//   }
+//  return message;
+// }
 
 $(document).ready(function(){
   $("form#teller").submit(function(event){
@@ -21,31 +27,57 @@ $(document).ready(function(){
     $("input:checkbox[name=event]:checked").each(function(){
       var fortuneEvents = $(this).val();
       fortuneSelections.push(fortuneEvents);
-      for (var i = 0; i<fortuneSelections.length; i++) {
-        // if (fortuneSelections.includes("black-cat", "ladder")) {
-        //   unluckyCount++;
-        //   alert(unluckyCount);
-        // } else if (fortuneSelections.includes("mirror", "toe")) {
-        //   unluckyCount++;
-        //   alert(unluckyCount);
-        // }
-        // if (fortuneSelections.includes("money", "rainbow")) {
-        //   luckyCount++;
-        // } else if (fortuneSelections.includes("clover", "winner")) {
-        //   luckyCount++;
-        // }
-        switch (fortuneSelections[i]) {
-          case 'black-cat':
-          alert("true");
-          case 'toe':
-          alert("toe");
-          break;
+      // fortuneSelections.forEach(function(counter) {
+      //   if (counter === "black-cat") {
+      //     unluckyCount++;
+      //     console.log(unluckyCount);
+      //   } else if (counter === "toe") {
+      //     unluckyCount++;
+      //     console.log(unluckyCount);
+      //   }
+      // });
+      for (var i = 0; i<1; i++) {
+        if (fortuneSelections.includes("black-cat")) {
+            unluckyCount++;
         }
+        if (fortuneSelections.includes("toe")) {
+            unluckyCount++;
+        }
+        if (fortuneSelections.includes("mirror")) {
+            unluckyCount++;
+        }
+        if (fortuneSelections.includes("ladder")) {
+            unluckyCount++;
+        }
+        if (fortuneSelections.includes("money")) {
+            luckyCount++;
+        }
+        if (fortuneSelections.includes("clover")) {
+            luckyCount++;
+        }
+        if (fortuneSelections.includes("rainbow")) {
+            luckyCount++;
+        }
+        if (fortuneSelections.includes("winner")) {
+            luckyCount++;
+            console.log(unluckyCount);
+            console.log(luckyCount);
+        }
+        // switch (fortuneSelections[i]) {
+        //   case 'black-cat':
+        //   unluckyCount++;
+        //   break;
+        //   case 'toe':
+        //   unluckyCount++;
+        //   break;
+        // }
       }
       $('#teller-responses').append(fortuneEvents + "<br>");
     });
-    var phrase = messageGenerator(luckyCount, unluckyCount);
-    $('#teller-responses').append(phrase + "<br>");
+    // var phrase = messageGenerator(luckyCount, unluckyCount);
+    console.log(unluckyCount);
+    console.log(luckyCount);
+    // $('#teller-responses').append(phrase + "<br>");
     $('#teller').hide();
   });
 });
